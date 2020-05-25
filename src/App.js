2dce6ee758f59132c00f75cfb627598e7b46ed8a
Amílcar Sánchez <45496427+amilcaralex97@users.noreply.github.com>
2020-05-25 12:13:35 -0500

    bootstrap integrated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./store";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navbar from "./components/navigation/Navbar";
 import { BrowserRouter } from "react-router-dom";
