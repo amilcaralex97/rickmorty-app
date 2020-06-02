c53277990df3152f97d36c63c830b256c6cff180
Amílcar Sánchez <45496427+amilcaralex97@users.noreply.github.com>
2020-06-02 17:15:28 -0500

    Link to characterList Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./store";
+import { BrowserRouter, Route } from "react-router-dom";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+
 import Navbar from "./components/navigation/Navbar";
-import { BrowserRouter } from "react-router-dom";
+import CharactersList from "./components/CharactersList";
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Navbar />
+        <Route exact path="/" component={CharactersList} />
       </BrowserRouter>
     </Provider>
   );
