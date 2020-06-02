5d3906d602477a0baae1dd5df1ce1163d0149b3e
Amílcar Sánchez <45496427+amilcaralex97@users.noreply.github.com>
2020-06-02 17:14:33 -0500

    added character reducer to combine reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import navbarReducer from "./navbarReducer";
+import charactersReducer from "./charactersReducer";
 
 export default combineReducers({
   navbarReducer,
+  charactersReducer,
 });
