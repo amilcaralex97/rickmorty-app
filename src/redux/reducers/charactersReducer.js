39e7ca3107e505c4411bf60a852bb47fd0b902b0
Amílcar Sánchez <45496427+amilcaralex97@users.noreply.github.com>
2020-06-08 14:30:34 -0500

    Filter functionality

--- src/redux/reducers/charactersReducer.js
+++ src/redux/reducers/charactersReducer.js
@@ -2,6 +2,7 @@ import {
   FETCH_CHARACTERS_PENDING,
   FETCH_CHARACTERS_SUCCESS,
   FETCH_CHARACTERS_ERROR,
+  FILTER_BY_VALUE,
 } from "../types/charactersTypes";
 
 const initialState = {
@@ -26,8 +27,21 @@ const charactersReducer = (state = { ...initialState }, action) => {
     case FETCH_CHARACTERS_ERROR:
       return {
         pending: false,
-        users: [],
+        characters: [],
         error: action.payload,
+      };
+    case FILTER_BY_VALUE:
+      let newState = Object.assign({}, state);
+
+      let value = action.payload.value;
+      let filteredValues = state.characters.filter((character) => {
+        return character.name.toLowerCase().includes(value);
+      });
+
+      let appliedFilters = state.appliedFilters;
+      return {
+        ...state,
+        characters: filteredValues,
       };
     default:
       return state;
